fa702afafc7053c76678208a1e2d0a3ab27f9082
zwados <lukasz.rozwadowski7@gmail.com>
2020-03-15 15:58:54 +0100

    Updates

---
+++ precache-manifest.76f636fe72127a30e0fdec67066307da.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3cb358008b10aca42fb42a758f11110",
+    "url": "/weatherly/index.html"
+  },
+  {
+    "revision": "0c440a3015687d025df4",
+    "url": "/weatherly/static/css/2.7abef457.chunk.css"
+  },
+  {
+    "revision": "5c2c3b3893dbb4034c25",
+    "url": "/weatherly/static/css/main.98b2ceac.chunk.css"
+  },
+  {
+    "revision": "0c440a3015687d025df4",
+    "url": "/weatherly/static/js/2.d1203b99.chunk.js"
+  },
+  {
+    "revision": "5c2c3b3893dbb4034c25",
+    "url": "/weatherly/static/js/main.168fbce5.chunk.js"
+  },
+  {
+    "revision": "1446218ee875776fc576",
+    "url": "/weatherly/static/js/runtime-main.a8a970fd.js"
+  },
+  {
+    "revision": "ee3e6d4a71235aacdb4850f4ee796b33",
+    "url": "/weatherly/static/media/global-bg.ee3e6d4a.jpg"
+  },
+  {
+    "revision": "b0732ec190a36d05b784c8ac375d6f67",
+    "url": "/weatherly/static/media/header.b0732ec1.jpg"
+  },
+  {
+    "revision": "be38a95c164a8fef123371a7e31fed6a",
+    "url": "/weatherly/static/media/sunny.be38a95c.png"
+  },
+  {
+    "revision": "d2e09b7b3d16755b6a0888fd8dc05f1c",
+    "url": "/weatherly/static/media/today.d2e09b7b.png"
+  },
+  {
+    "revision": "b106645c171dda8a644ba895fb53126b",
+    "url": "/weatherly/static/media/weather.b106645c.jpg"
+  }
+]);
